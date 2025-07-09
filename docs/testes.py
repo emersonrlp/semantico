fa394d4c7fd55463e9e2814c0d29070934bf32f5
Emerson Rodrigo Lima Pereira <emersonrodrigolimapereira@gmail.com>
2025-07-09 15:38:56 -0300

    feat: adicionando arquivo com os testes

---
+++ docs/testes.py
@@ -0,0 +1,25 @@
+'''teste de existencia
+
+class main {
+
+    const {  
+        int a;  
+    }
+    variables {
+        int soma;
+    }
+    t.soma = a;
+    toin.Idade = 10;
+    a = vetor[0] + 1;
+    vetor[30] = vetor[10] * 2--;
+    for(i; i < 5; i++){
+     toin.Idade = toin.Idade + i;
+     soma = toin->soma(toin.Idade, i) + i++;
+     teste = toin->teste();
+     print(toin->teste(), main->metodo2(), toin.Idade);
+     bla = ob -> soma(1,2);
+     teste = main->metodo2();
+    }
+    armario[1][1] = 9;
+}'''
+
